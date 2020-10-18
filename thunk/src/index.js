ce67b11e564085de53021d33048c5c137e809168
arkusa <askura@yeah.net>
2020-10-19 06:56:04 +0800

    style(thunk): 更新 thunk 🎉
    - 重命名 thunkify -> thunk
    - 更新文件结构 thunkify/index.js -> thunk/src/index.js
    - 代码格式化

--- thunk/src/index.js
+++ thunk/src/index.js
@@ -1,27 +1,26 @@
 /**
-* @file thunkify
-* @desc 生成thunk函数的工具方法
-* @author askura
-* @date 2020-10-19
-*/
+ * @file thunkify
+ * @desc 生成thunk函数的工具方法
+ * @author askura
+ * @date 2020-10-19
+ */
 
-function thunkify(func) {	
-
-	function acceptNextArgvs(...nextArgvs) {
+function thunkify(func) {
+  function acceptNextArgvs(...argvs) {
     const context = this;
 
     function acceptCallback(callback) {
-			try {
-				return func.call(context, ...argvs, applyOnce(callback));
-			} catch(err) {
-				return callback(err);
-			}
+      try {
+        return func.call(context, ...argvs, applyOnce(callback));
+      } catch (err) {
+        return callback(err);
+      }
     }
 
     return acceptCallback;
-	}
+  }
 
-	return acceptNextArgvs;
+  return acceptNextArgvs;
 }
 
 // 对回调函数需要加🔒, 保证只执行一次
@@ -43,14 +42,14 @@ function thunkify(func) {
 // 上面的🌰 会导致自动流程控制的yieldResult.value(next) 中的next被执行了2次
 // 从而导致generator流程⏩了1个流程
 function applyOnce(func) {
-	let called = false;
+  let called = false;
 
-	return function(...argvs) {
-		if (called) return;
+  return function (...argvs) {
+    if (called) return;
 
-		called = true;
-		func.call(this, ...argvs);
-	}
+    called = true;
+    func.call(this, ...argvs);
+  };
 }
 
 export default thunkify;
